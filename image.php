5d511ec50375ce4c93057bcbaaae8280fec3612b
aivijay <aivijay@gmail.com>
2018-08-16 14:08:04 -0500

    Update image.php

--- image.php
+++ image.php
@@ -15,7 +15,7 @@ if ( $deployment == 'blue') {
 
 // Draw a filled rectangle
 //imagefilledrectangle($im, 0, 0, 199, 199, $color);
-imagefilledrectangle($im, 0, 0, 299, 299, $color);
+imagefilledrectangle($im, 0, 0, 499, 499, $color);
 
 
 // Save the image
