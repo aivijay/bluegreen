f4e40b1eebb012e9a0cff35805ab94932e291fa9
aivijay <aivijay@gmail.com>
2018-08-16 14:02:39 -0500

    Update image.php

--- image.php
+++ image.php
@@ -3,7 +3,7 @@
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 
-echo "<b>Some test string</b>";
+echo "<b>Some test string ... test</b>";
 
 $deployment = getenv("COLOR");
 
